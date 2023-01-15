278c3549132012112194c59ee6c8e2bf0949b0f1
Karam Timani <sss484376@gmail.com>
2023-01-15 22:44:15 +0200

    First Commit setting us the screens and navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,15 @@
+
+import { View, Text, StyleSheet } from "react-native";
+
+function AllExpenses() {
+    return (
+        <View>
+            <Text>All Expense</Text>
+        </View>
+    )
+}
+export default AllExpenses;
+
+const styles = StyleSheet.create({
+
+})
---
+++ screens/ManageExpense.js
@@ -0,0 +1,15 @@
+
+import { View, Text, StyleSheet } from "react-native";
+
+function ManageExpense() {
+    return (
+        <View>
+            <Text>M Expense</Text>
+        </View>
+    )
+}
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+
+})
